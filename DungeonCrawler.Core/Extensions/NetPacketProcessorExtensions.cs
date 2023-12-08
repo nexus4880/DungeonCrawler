150a81ef08187ac458806dea9367fba5b48cd3fc
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2023-12-08 14:37:48 -0700

    added vector2 to packet serializers

--- DungeonCrawler.Core/Extensions/NetPacketProcessorExtensions.cs
+++ DungeonCrawler.Core/Extensions/NetPacketProcessorExtensions.cs
@@ -2,9 +2,12 @@
 
 namespace DungeonCrawler.Core.Extensions;
 
-public static class NetPacketProcessorExtensions {
-	public static void Initialize(this NetPacketProcessor packetProcessor) {
+public static class NetPacketProcessorExtensions
+{
+	public static void Initialize(this NetPacketProcessor packetProcessor)
+	{
 		packetProcessor.RegisterNestedType((writer, value) => writer.Put(value), reader => reader.GetGuid());
+		packetProcessor.RegisterNestedType((writer, value) => writer.Put(value), reader => reader.GetVector2());
 		packetProcessor.RegisterNestedType<PlayerInputs>();
 		packetProcessor.RegisterNestedType<DroppedLootItem>();
 	}
