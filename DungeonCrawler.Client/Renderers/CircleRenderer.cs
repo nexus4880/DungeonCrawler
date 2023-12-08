f57a5345a4181d7f36946fbb5e42d4c3ddb805d7
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2023-12-08 15:13:30 -0700

    forgot to push circlerenderer :)

--- DungeonCrawler.Client/Renderers/CircleRenderer.cs
+++ DungeonCrawler.Client/Renderers/CircleRenderer.cs
@@ -11,30 +11,24 @@ public class CircleRenderer : IRenderer
     public Entity Owner { get; init; }
 
     public Single Radius { get; set; }
-    public Vector2 Position { get; set; }
-
-    //public Action Draw { get; set; } I don't know if I want to do it like this, maybe an event?
 
     public void Deserialize(NetDataReader reader)
     {
         this.Radius = reader.GetFloat();
-        this.Position = reader.GetVector2();
     }
 
     public void Draw()
     {
-        DrawCircleLines((int)this.Position.X, (int)this.Position.Y, this.Radius, RED);
+        DrawCircleLines((int)this.Owner.Position.X, (int)this.Owner.Position.Y, this.Radius, RED);
     }
 
     public void Initialize(Queue properties)
     {
         this.Radius = properties.PopValue<Single>(8f);
-        this.Position = properties.PopValue<Vector2>(Vector2.Zero);
     }
 
     public void Serialize(NetDataWriter writer)
     {
         writer.Put(this.Radius);
-        writer.Put(this.Position);
     }
 }
