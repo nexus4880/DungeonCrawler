211a3386590564a50c16c6745217bc6bec2fad95
MaciejCwalina <150276683+MaciejCwalina@users.noreply.github.com>
2023-12-09 06:19:45 +0100

    asdfv

--- DungeonCrawler.Editor/Program.cs
+++ DungeonCrawler.Editor/Program.cs
@@ -1,8 +1,22 @@
-﻿namespace DungeonCrawler.Editor;
-
+﻿using static Raylib_CsLo.Raylib;
+using rlImGui_cs;
+using ImGuiNET;
 public class Program
 {
     public static void Main(string[] args)
     {
+        InitWindow(1280,720,"Editor");
+        rlImGui.Setup();
+        while(!WindowShouldClose()){
+            BeginDrawing();
+            ClearBackground(BLACK);
+            rlImGui.Begin();
+            ImGui.ShowDemoWindow();
+            rlImGui.End();
+            EndDrawing();
+        }
+
+        rlImGui.Shutdown();
+        CloseWindow();
     }
 }
