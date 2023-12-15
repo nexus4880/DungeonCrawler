332b1d14f9570103ba207c25b0dead3d072ecad3
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2023-12-14 22:50:57 -0700

    ffff

--- DungeonCrawler.Client/Renderers/ClientCircleRenderer.cs
+++ DungeonCrawler.Client/Renderers/ClientCircleRenderer.cs
@@ -9,7 +9,7 @@ public class ClientCircleRenderer : CircleRenderer
 {
     public override void Draw()
     {
-        Action<Int32, Int32, Single, Color> targetMethod = this.Filled ? DrawCircleLines : DrawCircle;
+        Action<Int32, Int32, Single, Color> targetMethod = this.Filled ? DrawCircle : DrawCircleLines;
         targetMethod((Int32)this.Owner.Position.X, (Int32)this.Owner.Position.Y, this.Radius, GetColor(this.Color));
     }
 }
